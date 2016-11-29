5aaa7ba6ade6597968523ef0df0044c85dd3f3ce
Miguel Torres <miguel.torres@ebury.com>
2016-11-29 10:50:52 +0100

    using carto from CDN in order to work with HTTPS

--- gulpfile.js
+++ gulpfile.js
@@ -166,7 +166,7 @@ gulp.task('default', ['clean'], () => {
   gulp.start('build');
 });
 
-gulp.task('deploy', ['default'], () => {
+gulp.task('deploy', ['build'], () => {
   return gulp.src('dist/**/*')
     .pipe($.ghPages());
 });
